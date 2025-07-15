6cfafec8a5e61d4583ce8641a12e5388c37d6b32
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-07-15 12:57:50 +0530

    new file:   ques1.java

---
+++ ques1.java
@@ -0,0 +1,10 @@
+//Write a Java program to print "Hello, World!" and explain the main components of the code.
+
+public class ques1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+//public class HelloWorld - Declares a class (filename must match: HelloWorld.java)
+//public static void main(String[] args) - Main method (entry point)/
+//System.out.println() - Prints text to console
