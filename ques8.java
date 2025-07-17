29f58287285d702d78353d32c8fabe7b618a38fd
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-07-17 20:51:43 +0530

    new file:   ques8.java 	new file:   ques9.java

---
+++ ques8.java
@@ -0,0 +1,19 @@
+//Write a Java program to calculate the factorial of a number.
+
+import java.util.Scanner;
+
+public class ques8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        long factorial = 1;
+        
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}
