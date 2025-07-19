3a56351043518b81288d1254b7520b9ccd3b401d
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-07-19 23:06:37 +0530

    new file:   Ques1_to_20/ques12.java 	new file:   Ques1_to_20/quest11.java

---
+++ Ques1_to_20/quest11.java
@@ -0,0 +1,21 @@
+package Ques1_to_20;
+
+//Find the Second Largest Number in an Array
+
+
+public class quest11 {
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+        System.out.println("Second largest: " + second);
+    }
+}
