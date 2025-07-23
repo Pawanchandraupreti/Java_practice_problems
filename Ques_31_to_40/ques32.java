1b813b37480bd4123401e3f3ae95e15d0acc93f5
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-07-23 21:18:43 +0530

    modified:   Ques_31_to_40/ques32.java

--- Ques_31_to_40/ques32.java
+++ Ques_31_to_40/ques32.java
@@ -1,7 +1,27 @@
 package Ques_31_to_40;
 
-
+//Array Utilities (Min/Max/Avg)
 
 public class ques32 {
-    
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+        for (int num : arr) {
+            if (num < min) min = num;
+        }
+        return min;
+    }
+
+    public static double calculateAverage(int[] arr) {
+        double sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 5};
+        System.out.println("Min: " + findMin(numbers));
+        System.out.println("Average: " + calculateAverage(numbers));
+    }
 }
