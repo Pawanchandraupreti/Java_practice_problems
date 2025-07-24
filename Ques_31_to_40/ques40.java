e3b0354a1c60a6d762598c288b6bf50eed5a0e59
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-07-24 21:24:27 +0530

    new file:   Ques_31_to_40/ques40.java

---
+++ Ques_31_to_40/ques40.java
@@ -0,0 +1,32 @@
+package Ques_31_to_40;
+
+//Encapsulation with Getters/Setters
+
+class BankAccount {
+    private double balance;
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+}
+
+public class ques40 {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(1000);
+        account.withdraw(500);
+        System.out.println("Current balance: " + account.getBalance());
+    }
+}
