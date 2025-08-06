6a4e2b07581f1ff126f7a344bb8c02bdfbad3096
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-06 23:06:55 +0530

    new file:   Ques_91_to_100/ques99.java

---
+++ Ques_91_to_100/ques99.java
@@ -0,0 +1,11 @@
+package Ques_91_to_100;
+
+// Count Words in a Sentence
+
+public class ques99 {
+    public static void main(String[] args) {
+        String sentence = "Java is an awesome programming language";
+        String[] words = sentence.split("\\s+");
+        System.out.println("Word count: " + words.length);
+    }
+}
