a63c2eadc69685b53444f230c9cb62b24dc90cd3
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-07 23:30:27 +0530

    new file:   Ques_101_to_110/ques105.java

---
+++ Ques_101_to_110/ques105.java
@@ -0,0 +1,31 @@
+package Ques_101_to_110;
+
+// Print Diamond Pattern
+
+public class ques105 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        // Upper half
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
