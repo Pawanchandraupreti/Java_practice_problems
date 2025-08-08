d789568d8adf19d6b3126e01932b17f875bd5a79
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-08 23:45:26 +0530

    new file:   Ques_101_to_110/ques108.java

---
+++ Ques_101_to_110/ques108.java
@@ -0,0 +1,17 @@
+package Ques_101_to_110;
+
+// Count Character Occurrences
+
+public class ques108 {
+    public static void main(String[] args) {
+        String text = "programming";
+        char target = 'm';
+        int count = 0;
+        
+        for (char c : text.toCharArray()) {
+            if (c == target) count++;
+        }
+        
+        System.out.println(target + " appears " + count + " times");
+    }
+}
