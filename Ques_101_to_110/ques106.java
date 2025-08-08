bf53a2940e75e3127d9fab45719b78f5a7bd3e98
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-08 23:43:44 +0530

    new file:   Ques_101_to_110/ques106.java

---
+++ Ques_101_to_110/ques106.java
@@ -0,0 +1,19 @@
+package Ques_101_to_110;
+
+// Anagram Checker (Case-Insensitive)
+
+public class ques106 {
+    public static void main(String[] args) {
+        String str1 = "Listen";
+        String str2 = "Silent";
+        
+        char[] arr1 = str1.toLowerCase().toCharArray();
+        char[] arr2 = str2.toLowerCase().toCharArray();
+        
+        java.util.Arrays.sort(arr1);
+        java.util.Arrays.sort(arr2);
+        
+        boolean isAnagram = java.util.Arrays.equals(arr1, arr2);
+        System.out.println("Are anagrams? " + isAnagram);
+    }
+}
