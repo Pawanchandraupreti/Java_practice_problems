dc6cedc61923263b4651c21a01d433c049d190fe
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-09 23:52:02 +0530

    modified:   Ques_101_to_110/ques102.java

--- Ques_101_to_110/ques102.java
+++ Ques_101_to_110/ques102.java
@@ -9,8 +9,10 @@ public class ques102 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter principal amount: ");
         double principal = sc.nextDouble();
+
         System.out.print("Enter rate of interest: ");
         double rate = sc.nextDouble();
+        
         System.out.print("Enter time (years): ");
         double time = sc.nextDouble();
         
