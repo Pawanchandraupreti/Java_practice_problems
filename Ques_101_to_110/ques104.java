56e5697f034816e9e95275ba2ac829d7dff89907
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-09 23:51:32 +0530

    modified:   Ques_101_to_110/ques104.java

--- Ques_101_to_110/ques104.java
+++ Ques_101_to_110/ques104.java
@@ -5,14 +5,12 @@ package Ques_101_to_110;
 public class ques104 {
     public static void main(String[] args) {
         int num1 = 54, num2 = 24;
-        
         while (num1 != num2) {
             if (num1 > num2)
                 num1 -= num2;
             else
                 num2 -= num1;
         }
-        
         System.out.println("GCD: " + num1);
     }
 }
