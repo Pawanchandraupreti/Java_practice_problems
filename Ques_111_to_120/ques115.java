0ef74ecf972e683b031f3b6a8d97c7d6441366a4
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-10 23:21:59 +0530

    new file:   Ques_111_to_120/ques115.java

---
+++ Ques_111_to_120/ques115.java
@@ -0,0 +1,20 @@
+package Ques_111_to_120;
+
+// Print Pascal's Triangle
+
+public class ques115 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 0; i < rows; i++) {
+            int number = 1;
+            System.out.format("%" + (rows - i) * 2 + "s", "");
+            
+            for (int j = 0; j <= i; j++) {
+                System.out.format("%4d", number);
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
