caa8a80cb43eca5cba1f5a7dd8a90636b19e6d19
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-10 23:16:05 +0530

    new file:   Ques_111_to_120/ques111.java

---
+++ Ques_111_to_120/ques111.java
@@ -0,0 +1,17 @@
+package Ques_111_to_120;
+
+// Find Missing Number in Sequence (1-100)
+
+public class ques111 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10, /*...*/ 99}; // Missing 100
+        int expectedSum = 100 * 101 / 2;
+        int actualSum = 0;
+        
+        for (int num : nums) {
+            actualSum += num;
+        }
+        
+        System.out.println("Missing number: " + (expectedSum - actualSum));
+    }
+}
