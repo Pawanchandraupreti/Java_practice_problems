144c70124a1be39fee1c4288fea25377f9dded4d
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-10 23:22:29 +0530

    modified:   Ques_111_to_120/ques111.java

--- Ques_111_to_120/ques111.java
+++ Ques_111_to_120/ques111.java
@@ -7,7 +7,6 @@ public class ques111 {
         int[] nums = {1,2,3,4,5,6,7,8,9,10, /*...*/ 99}; // Missing 100
         int expectedSum = 100 * 101 / 2;
         int actualSum = 0;
-        
         for (int num : nums) {
             actualSum += num;
         }
