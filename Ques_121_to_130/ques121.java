532f43cb9d6ba4ac2d166c1a0d4766c88cd6f09b
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-12 23:08:44 +0530

    modified:   Ques_121_to_130/ques121.java

--- Ques_121_to_130/ques121.java
+++ Ques_121_to_130/ques121.java
@@ -1,7 +1,17 @@
 package Ques_121_to_130;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 // Calculate Age from Birthdate
 
 public class ques121 {
-    
+    public static void main(String[] args) {
+        LocalDate birthdate = LocalDate.of(1990, 5, 15);
+        LocalDate today = LocalDate.now();
+        
+        Period age = Period.between(birthdate, today);
+        System.out.printf("Age: %d years, %d months, %d days", 
+                         age.getYears(), age.getMonths(), age.getDays());
+    }
 }
