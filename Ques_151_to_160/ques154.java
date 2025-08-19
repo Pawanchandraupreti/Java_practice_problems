d7fc83becc3c30294245e914817f16cab333d0b7
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-19 23:04:42 +0530

    new file:   Ques_151_to_160/ques154.java

---
+++ Ques_151_to_160/ques154.java
@@ -0,0 +1,23 @@
+package Ques_151_to_160;
+
+// Generate a Multiplication Table Grid
+
+public class ques154 {
+    public static void main(String[] args) {
+        int size = 5;
+        
+        System.out.print("    ");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println("\n    " + "====".repeat(size));
+        
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%2d |", i);
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
