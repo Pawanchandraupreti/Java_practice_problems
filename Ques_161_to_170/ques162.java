66f78cd2d7ca3accf4377aa5cdd5e451986e13f4
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-21 23:22:09 +0530

    new file:   Ques_161_to_170/ques162.java

---
+++ Ques_161_to_170/ques162.java
@@ -0,0 +1,22 @@
+package Ques_161_to_170;
+
+import java.util.HashMap;
+
+// Create a Word Frequency Counter
+
+public class ques162 {
+    public static void main(String[] args) {
+        String text = "hello world hello java world programming";
+        String[] words = text.split(" ");
+        HashMap<String, Integer> frequency = new HashMap<>();
+        
+        for (String word : words) {
+            frequency.put(word, frequency.getOrDefault(word, 0) + 1);
+        }
+        
+        System.out.println("Word frequencies:");
+        for (String word : frequency.keySet()) {
+            System.out.println(word + ": " + frequency.get(word));
+        }
+    }
+}
