f1bb7ab6b69ab304ae5652e8fdcc0a37ad267c82
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-21 23:24:32 +0530

    new file:   Ques_161_to_170/ques164.java

---
+++ Ques_161_to_170/ques164.java
@@ -0,0 +1,15 @@
+package Ques_161_to_170;
+
+// Create a Simple Text-Based Animation
+
+public class ques164 {
+    public static void main(String[] args) throws InterruptedException {
+        String[] frames = {"-", "\\", "|", "/"};
+        
+        for (int i = 0; i < 20; i++) {
+            System.out.print("\rLoading " + frames[i % frames.length]);
+            Thread.sleep(200);
+        }
+        System.out.println("\rDone!          ");
+    }
+}
