0afe08d087acca9af3c2304a0350dd1ed46988e5
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-21 23:20:51 +0530

    new file:   Ques_161_to_170/ques161.java

---
+++ Ques_161_to_170/ques161.java
@@ -0,0 +1,18 @@
+package Ques_161_to_170;
+
+// Generate a Christmas Tree Pattern
+
+public class ques161 {
+    public static void main(String[] args) {
+        int height = 6;
+        
+        for (int i = 0; i < height; i++) {
+            System.out.print(" ".repeat(height - i - 1));
+            System.out.println("*".repeat(2 * i + 1));
+        }
+        
+        // Trunk
+        System.out.print(" ".repeat(height - 2));
+        System.out.println("|||");
+    }
+}
