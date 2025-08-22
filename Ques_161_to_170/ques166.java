fecc5b11f1f2d82e38c7e97a42842bc748b94dd4
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-22 23:10:48 +0530

    new file:   Ques_161_to_170/ques166.java

---
+++ Ques_161_to_170/ques166.java
@@ -0,0 +1,20 @@
+package Ques_161_to_170;
+
+// Generate a Pascal's Triangle
+
+public class ques166 {
+    public static void main(String[] args) {
+        int rows = 6;
+        
+        for (int i = 0; i < rows; i++) {
+            int number = 1;
+            System.out.print(" ".repeat(rows - i));
+            
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
