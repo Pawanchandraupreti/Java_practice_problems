93a61f9129868c34378f28cd17732eadbe90b1ec
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-22 23:16:40 +0530

    new file:   Ques_161_to_170/ques170.java

---
+++ Ques_161_to_170/ques170.java
@@ -0,0 +1,18 @@
+package Ques_161_to_170;
+
+// Calculate the Sum of Natural Numbers Using Recursion
+
+public class ques170 {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = calculateSum(n);
+        System.out.println("Sum of first " + n + " natural numbers: " + sum);
+    }
+    
+    static int calculateSum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + calculateSum(n - 1);
+    }
+}
