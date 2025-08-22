9c953f66304c5a82bd1d40eb1804c262061f1df6
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-22 23:13:01 +0530

    new file:   Ques_161_to_170/ques167.java

---
+++ Ques_161_to_170/ques167.java
@@ -0,0 +1,17 @@
+package Ques_161_to_170;
+
+import java.util.Scanner;
+
+// Create a Simple Unit Converter (Miles to Kilometers)
+
+public class ques167 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter distance in miles: ");
+        double miles = scanner.nextDouble();
+        
+        double kilometers = miles * 1.60934;
+        System.out.printf("%.2f miles = %.2f kilometers%n", miles, kilometers);
+    }
+}
