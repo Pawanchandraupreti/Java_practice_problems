2bd70e58035c3892bb014582786e6ddd471a7588
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-23 23:21:23 +0530

    modified:   Ques_161_to_170/ques166.java

--- Ques_161_to_170/ques166.java
+++ Ques_161_to_170/ques166.java
@@ -9,7 +9,6 @@ public class ques166 {
         for (int i = 0; i < rows; i++) {
             int number = 1;
             System.out.print(" ".repeat(rows - i));
-            
             for (int j = 0; j <= i; j++) {
                 System.out.print(number + " ");
                 number = number * (i - j) / (j + 1);
