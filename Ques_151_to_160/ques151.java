fd81efde06acc99ee80823938438c6f1231dbe71
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-24 22:50:45 +0530

    modified:   Ques_151_to_160/ques151.java

--- Ques_151_to_160/ques151.java
+++ Ques_151_to_160/ques151.java
@@ -6,7 +6,6 @@ public class ques151 {
     public static void main(String[] args) {
         String password = "Secure123!";
         int strength = 0;
-        
         if (password.length() >= 8) strength++;
         if (password.matches(".*[A-Z].*")) strength++;
         if (password.matches(".*[a-z].*")) strength++;
