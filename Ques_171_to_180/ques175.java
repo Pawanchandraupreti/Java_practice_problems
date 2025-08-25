f7624e5f3abb26605488ca9962b6e3f512187491
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-25 23:10:46 +0530

    new file:   Ques_171_to_180/ques175.java

---
+++ Ques_171_to_180/ques175.java
@@ -0,0 +1,18 @@
+package Ques_171_to_180;
+
+// Calculate the Factorial Using Recursion
+
+public class ques175 {
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+    
+    static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * calculateFactorial(n - 1);
+    }
+}
