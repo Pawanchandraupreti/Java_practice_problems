ab5ac7311d519067f6f4bc433b90b924e4052cb9
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-25 23:09:43 +0530

    new file:   Ques_171_to_180/ques174.java

---
+++ Ques_171_to_180/ques174.java
@@ -0,0 +1,25 @@
+package Ques_171_to_180;
+
+import java.util.Random;
+import java.util.Scanner;
+
+// Create a Simple Text-Based Roulette Game
+
+public class ques174 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Guess a number between 0 and 36: ");
+        int guess = scanner.nextInt();
+        
+        int result = rand.nextInt(37);
+        System.out.println("The wheel spins... " + result + "!");
+        
+        if (guess == result) {
+            System.out.println("Congratulations! You won!");
+        } else {
+            System.out.println("Sorry, better luck next time!");
+        }
+    }
+}
