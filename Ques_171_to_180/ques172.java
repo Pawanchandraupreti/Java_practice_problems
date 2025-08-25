e30ef49095f538a94502085cb6abe3d00770d3ce
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-25 23:07:07 +0530

    new file:   Ques_171_to_180/ques172.java

---
+++ Ques_171_to_180/ques172.java
@@ -0,0 +1,23 @@
+package Ques_171_to_180;
+
+// Create a Prime Number Checker
+
+public class ques172 {
+    public static void main(String[] args) {
+        int number = 29;
+        boolean isPrime = true;
+        
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        
+        System.out.println(number + " is " + (isPrime ? "prime" : "not prime"));
+    }
+}
