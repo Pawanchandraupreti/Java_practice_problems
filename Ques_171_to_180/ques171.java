5d5b59f91908a3e881ce6d1daafa18f0eae21897
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-25 23:05:36 +0530

    new file:   Ques_171_to_180/ques171.java

---
+++ Ques_171_to_180/ques171.java
@@ -0,0 +1,17 @@
+package Ques_171_to_180;
+
+// Generate a Word Triangle
+
+public class ques171 {
+    public static void main(String[] args) {
+        String word = "TRIANGLE";
+        
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.substring(0, i + 1));
+        }
+        
+        for (int i = word.length() - 2; i >= 0; i--) {
+            System.out.println(word.substring(0, i + 1));
+        }
+    }
+}
