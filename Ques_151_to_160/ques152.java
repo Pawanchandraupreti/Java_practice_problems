d9692f2fb25b606371912bd5f4799bc9e47573e5
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-28 23:09:41 +0530

    modified:   Ques_151_to_160/ques152.java

--- Ques_151_to_160/ques152.java
+++ Ques_151_to_160/ques152.java
@@ -6,6 +6,7 @@ public class ques152 {
     public static void main(String[] args) throws InterruptedException {
         int total = 20;  
         for (int i = 0; i <= total; i++) {
+            
             int percent = i * 100 / total;
             String bar = "[" + "=".repeat(i) + " ".repeat(total - i) + "]";
             System.out.printf("\r%d%% %s", percent, bar);
