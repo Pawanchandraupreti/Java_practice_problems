f32b820310e640403a6ae087ee83bb34c012cf48
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-30 23:10:15 +0530

    new file:   Ques_171_to_180/ques177.java

---
+++ Ques_171_to_180/ques177.java
@@ -0,0 +1,20 @@
+package Ques_171_to_180;
+
+// Generate a Pattern of Alternating Letters
+
+public class ques177 {
+    public static void main(String[] args) {
+        int size = 5;
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("A ");
+                } else {
+                    System.out.print("B ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
