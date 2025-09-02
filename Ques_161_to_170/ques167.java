9079eba4bf6afa0568c5206e527fe86bc994ebb2
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-02 23:24:49 +0530

    modified:   Ques_161_to_170/ques167.java

--- Ques_161_to_170/ques167.java
+++ Ques_161_to_170/ques167.java
@@ -8,6 +8,7 @@ public class ques167 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter distance in miles: ");
+        
         double miles = scanner.nextDouble();
         double kilometers = miles * 1.60934;
         System.out.printf("%.2f miles = %.2f kilometers%n", miles, kilometers);
