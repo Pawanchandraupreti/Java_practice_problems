95a5918ea027dfda88ceb3ed4e158609b9f17a40
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-04 23:15:42 +0530

    new file:   Ques_181_to_190/ques181.java

---
+++ Ques_181_to_190/ques181.java
@@ -0,0 +1,13 @@
+package Ques_181_to_190;
+
+//Generate a Word Ladder Pattern
+
+public class ques181 {
+    public static void main(String[] args) {
+        String word = "LADDER";
+        
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.substring(i) + word.substring(0, i));
+        }
+    }
+}
