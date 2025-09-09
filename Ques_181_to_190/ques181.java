fdfe79108c6ab298674223c6bfe7b00f067b04d7
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-09 23:25:11 +0530

    modified:   Ques_181_to_190/ques181.java

--- Ques_181_to_190/ques181.java
+++ Ques_181_to_190/ques181.java
@@ -6,6 +6,8 @@ public class ques181 {
     public static void main(String[] args) {
         String word = "LADDER";
         
+
+        
         for (int i = 0; i < word.length(); i++) {
             System.out.println(word.substring(i) + word.substring(0, i));
         }
