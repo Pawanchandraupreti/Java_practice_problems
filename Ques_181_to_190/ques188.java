8b545778a4d24235f4a66b77d8d9e52b874325d5
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-10 23:31:31 +0530

    new file:   Ques_181_to_190/ques188.java

---
+++ Ques_181_to_190/ques188.java
@@ -0,0 +1,17 @@
+package Ques_181_to_190;
+
+// Calculate the Digital Product of a Number
+
+public class ques188 {
+    public static void main(String[] args) {
+        int number = 234;
+        int product = 1;
+        
+        while (number > 0) {
+            product *= number % 10;
+            number /= 10;
+        }
+        
+        System.out.println("Digital product: " + product);
+    }
+}
