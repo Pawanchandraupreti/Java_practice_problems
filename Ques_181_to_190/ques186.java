10c4e3a36c4fbcb458f676146b52916ae1063918
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-13 22:54:41 +0530

    modified:   Ques_181_to_190/ques186.java

--- Ques_181_to_190/ques186.java
+++ Ques_181_to_190/ques186.java
@@ -7,6 +7,7 @@ public class ques186 {
         String word = "PYRAMID";
         int length = word.length();
         
+        
         for (int i = 0; i < length; i++) {
             String line = word.substring(0, i + 1);
             System.out.println(" ".repeat(length - i - 1) + line);
