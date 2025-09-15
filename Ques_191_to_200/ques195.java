28f82bbf7adfa326efb3283cbb3682b90d28bf57
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-15 23:32:19 +0530

    new file:   Ques_191_to_200/ques195.java

---
+++ Ques_191_to_200/ques195.java
@@ -0,0 +1,20 @@
+package Ques_191_to_200;
+
+// Generate a Pattern of Alternating Numbers and Stars
+
+public class ques195 {
+    public static void main(String[] args) {
+        int n = 5;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j % 2 == 1) {
+                    System.out.print(j + " ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
