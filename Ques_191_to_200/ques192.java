732487b2e4aa3b499df0356e5e8c853af94eea73
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-16 23:28:19 +0530

    modified:   Ques_191_to_200/ques192.java

--- Ques_191_to_200/ques192.java
+++ Ques_191_to_200/ques192.java
@@ -9,6 +9,7 @@ public class ques192 {
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
+                
             }
             System.out.println();
         }
