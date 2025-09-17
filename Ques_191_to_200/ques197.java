cada72aeb1aba7960e63021eb70a8e3a2a2729ca
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-17 23:12:24 +0530

    new file:   Ques_191_to_200/ques197.java

---
+++ Ques_191_to_200/ques197.java
@@ -0,0 +1,18 @@
+package Ques_191_to_200;
+
+// Create a Fibonacci Sequence Generator
+
+public class ques197 {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0, b = 1;
+        
+        System.out.print("Fibonacci sequence: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
