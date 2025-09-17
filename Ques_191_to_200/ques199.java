991b04bdc7d7f2f7eed441a97d99cdec25c3d617
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-17 23:15:02 +0530

    new file:   Ques_191_to_200/ques199.java

---
+++ Ques_191_to_200/ques199.java
@@ -0,0 +1,17 @@
+package Ques_191_to_200;
+
+// Generate a Pattern of Numbers in a Square
+
+public class ques199 {
+    public static void main(String[] args) {
+        int size = 4;
+        int count = 1;
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%3d", count++);
+            }
+            System.out.println();
+        }
+    }
+}
