0cc62fdbf9f646eb5bfdfab3275f56df98cc9975
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-17 23:11:01 +0530

    new file:   Ques_191_to_200/ques196.java

---
+++ Ques_191_to_200/ques196.java
@@ -0,0 +1,13 @@
+package Ques_191_to_200;
+
+// Generate a Word Reverse Pattern
+
+public class ques196 {
+    public static void main(String[] args) {
+        String word = "PATTERN";
+        
+        for (int i = word.length(); i >= 1; i--) {
+            System.out.println(word.substring(0, i));
+        }
+    }
+}
