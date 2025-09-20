ff004e4cf23011a9dcbaf7d9c163e220d764208b
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-20 23:56:36 +0530

    modified:   Ques_191_to_200/ques197.java

--- Ques_191_to_200/ques197.java
+++ Ques_191_to_200/ques197.java
@@ -7,6 +7,7 @@ public class ques197 {
         int n = 10;
         int a = 0, b = 1;
         
+        
         System.out.print("Fibonacci sequence: ");
         for (int i = 0; i < n; i++) {
             System.out.print(a + " ");
