92aa1b45d2bd7eb0d700d3871f02cc49b4cf9f48
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-21 23:22:35 +0530

    new file:   Ques_201_to_210/ques204.java

---
+++ Ques_201_to_210/ques204.java
@@ -0,0 +1,17 @@
+package Ques_201_to_210;
+
+// Calculate the Product of Digits in a Number
+
+public class ques204 {
+    public static void main(String[] args) {
+        int number = 2345;
+        int product = 1;
+        
+        while (number > 0) {
+            product *= number % 10;
+            number /= 10;
+        }
+        
+        System.out.println("Product of digits: " + product);
+    }
+}
