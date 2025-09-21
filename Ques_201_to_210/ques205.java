9b99b7f1a5bfd5ccef903fbc845601cd5776d2e8
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-21 23:23:16 +0530

    new file:   Ques_201_to_210/ques205.java

---
+++ Ques_201_to_210/ques205.java
@@ -0,0 +1,22 @@
+package Ques_201_to_210;
+
+import java.util.Random;
+
+// Create a Simple Text-Based Lottery System
+
+public class ques205 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int winningNumber = rand.nextInt(100) + 1;
+        
+        System.out.println("Lottery Draw!");
+        System.out.println("The winning number is: " + winningNumber);
+        
+        // Simulate some tickets
+        for (int i = 1; i <= 5; i++) {
+            int ticket = rand.nextInt(100) + 1;
+            System.out.println("Ticket " + i + ": " + ticket + 
+                             (ticket == winningNumber ? " - WINNER!" : ""));
+        }
+    }
+}
