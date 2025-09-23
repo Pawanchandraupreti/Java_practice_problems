3c645da233adc0e24281d8914eef3c4c09be5091
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-23 23:25:17 +0530

    new file:   Ques_201_to_210/ques207.java

---
+++ Ques_201_to_210/ques207.java
@@ -0,0 +1,16 @@
+package Ques_201_to_210;
+
+// Generate a Pattern of Numbers in Reverse Order
+
+public class ques207 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = rows; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
