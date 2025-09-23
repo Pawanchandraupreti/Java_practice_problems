5b8576dd0e4e5d2a13475f321b0937492f29c24d
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-23 23:26:35 +0530

    new file:   Ques_201_to_210/ques208.java

---
+++ Ques_201_to_210/ques208.java
@@ -0,0 +1,24 @@
+package Ques_201_to_210;
+
+// Calculate the Standard Deviation of Numbers
+
+public class ques208 {
+    public static void main(String[] args) {
+        double[] numbers = {10, 12, 23, 23, 16, 23, 21, 16};
+        double sum = 0;
+        
+        for (double num : numbers) {
+            sum += num;
+        }
+        double mean = sum / numbers.length;
+        
+        double variance = 0;
+        for (double num : numbers) {
+            variance += Math.pow(num - mean, 2);
+        }
+        variance /= numbers.length;
+        
+        double stdDev = Math.sqrt(variance);
+        System.out.printf("Standard Deviation: %.2f%n", stdDev);
+    }
+}
