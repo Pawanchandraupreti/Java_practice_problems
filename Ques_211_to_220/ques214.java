173fd96a62764dae04cfba206af9cbcf9c7da051
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-25 23:32:17 +0530

    new file:   Ques_211_to_220/ques214.java

---
+++ Ques_211_to_220/ques214.java
@@ -0,0 +1,17 @@
+package Ques_211_to_220;
+
+// Calculate the Harmonic Series Sum
+
+public class ques214 {
+    public static void main(String[] args) {
+        int n = 5;
+        double sum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+            System.out.printf("1/%d = %.4f%n", i, 1.0/i);
+        }
+        
+        System.out.printf("Sum of first %d terms: %.4f%n", n, sum);
+    }
+}
