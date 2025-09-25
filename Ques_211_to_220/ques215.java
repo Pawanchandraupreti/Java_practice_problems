67074cc34174d8f33ea522bf22a66f296e242733
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-25 23:33:44 +0530

    new file:   Ques_211_to_220/ques215.java

---
+++ Ques_211_to_220/ques215.java
@@ -0,0 +1,24 @@
+package Ques_211_to_220;
+
+import java.util.Random;
+import java.util.Scanner;
+
+// Create a Simple Text-Based Magic 8-Ball
+
+public class ques215 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        String[] answers = {
+            "Yes, definitely", "No, definitely not", "Maybe", 
+            "Ask again later", "Outlook good", "Outlook not so good",
+            "Cannot predict now", "Signs point to yes"
+        };
+        
+        System.out.print("Ask the Magic 8-Ball a question: ");
+        scanner.nextLine(); // Read the question
+        
+        String answer = answers[rand.nextInt(answers.length)];
+        System.out.println("Magic 8-Ball says: " + answer);
+    }
+}
