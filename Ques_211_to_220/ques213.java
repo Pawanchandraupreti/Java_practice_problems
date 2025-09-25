34c63990a99a7803e01c0a26b2cedb53b5d39eb5
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-25 23:31:04 +0530

    new file:   Ques_211_to_220/ques213.java

---
+++ Ques_211_to_220/ques213.java
@@ -0,0 +1,18 @@
+package Ques_211_to_220;
+
+// Generate a Pattern of Numbers in a Pyramid with Spaces
+
+public class ques213 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(" ".repeat(rows - i));
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
