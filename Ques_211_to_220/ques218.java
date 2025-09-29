7d5b2ba651911b3046dcd04fd608fcfa87170bfc
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-29 23:34:04 +0530

    new file:   Ques_211_to_220/ques218.java

---
+++ Ques_211_to_220/ques218.java
@@ -0,0 +1,22 @@
+package Ques_211_to_220;
+
+import java.util.Arrays;
+
+// Calculate the Median of an Array
+
+public class ques218 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 1, 4, 2, 5, 6, 7};
+        Arrays.sort(numbers);
+        
+        double median;
+        if (numbers.length % 2 == 0) {
+            median = (numbers[numbers.length/2 - 1] + numbers[numbers.length/2]) / 2.0;
+        } else {
+            median = numbers[numbers.length/2];
+        }
+        
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+        System.out.println("Median: " + median);
+    }
+}
