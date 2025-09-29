98802869487956c75d4671635848c369ece5b9a4
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-29 23:33:04 +0530

    new file:   Ques_211_to_220/ques217.java

---
+++ Ques_211_to_220/ques217.java
@@ -0,0 +1,32 @@
+package Ques_211_to_220;
+
+// Generate a Pattern of Letters in a Diamond Shape
+
+public class ques217 {
+    public static void main(String[] args) {
+        int n = 4;
+        char currentChar = 'A';
+        
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            System.out.print(" ".repeat(n - i));
+            for (int j = 1; j <= i; j++) {
+                System.out.print(currentChar + " ");
+                currentChar++;
+            }
+            System.out.println();
+            currentChar = 'A';
+        }
+        
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            System.out.print(" ".repeat(n - i));
+            for (int j = 1; j <= i; j++) {
+                System.out.print(currentChar + " ");
+                currentChar++;
+            }
+            System.out.println();
+            currentChar = 'A';
+        }
+    }
+}
