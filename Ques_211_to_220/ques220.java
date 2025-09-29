29fd454b740d171551ad2041270b21aecb2f449c
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-29 23:36:01 +0530

    new file:   Ques_211_to_220/ques220.java

---
+++ Ques_211_to_220/ques220.java
@@ -0,0 +1,16 @@
+package Ques_211_to_220;
+
+// Generate a Pattern of Binary Numbers
+
+public class ques220 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j % 2 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
