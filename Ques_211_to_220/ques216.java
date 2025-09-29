e70233e593cc1a5462ce9b417500ecb1893a95b8
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-09-29 23:31:48 +0530

    new file:   Ques_211_to_220/ques216.java

---
+++ Ques_211_to_220/ques216.java
@@ -0,0 +1,24 @@
+package Ques_211_to_220;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Find the Intersection of Two Arrays
+
+public class ques216 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {4, 5, 6, 7, 8};
+        Set<Integer> intersection = new HashSet<>();
+        
+        for (int num1 : arr1) {
+            for (int num2 : arr2) {
+                if (num1 == num2) {
+                    intersection.add(num1);
+                }
+            }
+        }
+        
+        System.out.println("Intersection: " + intersection);
+    }
+}
