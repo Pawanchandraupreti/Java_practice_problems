d3676918909c98b3841975f90f892bb0ff8f0f3c
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-02 23:01:18 +0530

    modified:   Ques_211_to_220/ques218.java

--- Ques_211_to_220/ques218.java
+++ Ques_211_to_220/ques218.java
@@ -16,7 +16,6 @@ public class ques218 {
             median = numbers[numbers.length/2];
         }
         
-        
         System.out.println("Sorted array: " + Arrays.toString(numbers));
         System.out.println("Median: " + median);
     }
