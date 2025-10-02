22e366e7f308baaa5abf44a59e3a98583fc3aeeb
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-02 23:01:34 +0530

    modified:   Ques_211_to_220/ques217.java

--- Ques_211_to_220/ques217.java
+++ Ques_211_to_220/ques217.java
@@ -18,7 +18,6 @@ public class ques217 {
             currentChar = 'A';
         }
         
-        
         // Lower half
         for (int i = n - 1; i >= 1; i--) {
             System.out.print(" ".repeat(n - i));
