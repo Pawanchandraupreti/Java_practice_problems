99dc4d5802e80a6c559fcf7b6ee3133eac4ab43a
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-06 21:10:43 +0530

    new file:   Ques_221_to_230/ques225.java

---
+++ Ques_221_to_230/ques225.java
@@ -0,0 +1,27 @@
+package Ques_221_to_230;
+
+import java.util.Random;
+
+// Create a Simple Text-Based Fortune Cookie
+
+public class ques225 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        String[] fortunes = {
+            "You will have a great day today!",
+            "A surprise is waiting for you around the corner.",
+            "Good news will come to you by mail.",
+            "You will travel to many places.",
+            "Your hard work will soon pay off.",
+            "A dream you have will come true.",
+            "Someone is thinking of you."
+        };
+        
+        String fortune = fortunes[rand.nextInt(fortunes.length)];
+        System.out.println("Your fortune: " + fortune);
+        System.out.println("Lucky numbers: " + 
+            (rand.nextInt(99) + 1) + ", " + 
+            (rand.nextInt(99) + 1) + ", " + 
+            (rand.nextInt(99) + 1));
+    }
+}
