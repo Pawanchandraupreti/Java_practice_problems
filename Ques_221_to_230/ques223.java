6915b8dd302f7cadc1ca524c02a8f68388e4db9c
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-06 21:08:52 +0530

    new file:   Ques_221_to_230/ques223.java

---
+++ Ques_221_to_230/ques223.java
@@ -0,0 +1,22 @@
+package Ques_221_to_230;
+
+// Generate a Pattern of Numbers in a Hollow Square
+
+public class ques223 {
+    public static void main(String[] args) {
+        int size = 5;
+        int count = 1;
+        
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size || j == 1 || j == size) {
+                    System.out.printf("%2d", count++);
+                } else {
+                    System.out.print("  ");
+                    count++;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
