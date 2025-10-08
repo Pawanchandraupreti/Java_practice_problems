64109ed6075ef12be8ec354faad3e30619d63472
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-08 23:07:15 +0530

    new file:   Ques_221_to_230/ques227.java

---
+++ Ques_221_to_230/ques227.java
@@ -0,0 +1,17 @@
+package Ques_221_to_230;
+
+// Generate a Pattern of Numbers in a Right-Aligned Triangle
+
+public class ques227 {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(" ".repeat(rows - i));
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
