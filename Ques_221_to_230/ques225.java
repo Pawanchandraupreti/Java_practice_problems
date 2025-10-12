4ed88e7dac151be5b532fec57efc64b5bab63ae2
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-12 22:57:28 +0530

    modified:   Ques_221_to_230/ques225.java

--- Ques_221_to_230/ques225.java
+++ Ques_221_to_230/ques225.java
@@ -17,6 +17,7 @@ public class ques225 {
             "Someone is thinking of you."
         };
         
+        
         String fortune = fortunes[rand.nextInt(fortunes.length)];
         System.out.println("Your fortune: " + fortune);
         System.out.println("Lucky numbers: " + 
