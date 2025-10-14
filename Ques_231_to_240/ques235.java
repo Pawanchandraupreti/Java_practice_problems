c819045725526a4ce9fc4aa51e8290344e0fe85e
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-14 23:22:54 +0530

    new file:   Ques_231_to_240/ques235.java

---
+++ Ques_231_to_240/ques235.java
@@ -0,0 +1,27 @@
+package Ques_231_to_240;
+
+import java.util.Random;
+
+// Create a Simple Text-Based Dice Roll Statistics
+
+public class ques235 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] rolls = new int[100];
+        int[] frequency = new int[6];
+        
+        // Roll dice 100 times
+        for (int i = 0; i < 100; i++) {
+            int roll = rand.nextInt(6) + 1;
+            rolls[i] = roll;
+            frequency[roll - 1]++;
+        }
+        
+        // Display statistics
+        System.out.println("Dice Roll Statistics (100 rolls):");
+        for (int i = 0; i < 6; i++) {
+            System.out.printf("Number %d: %d times (%.1f%%)%n", 
+                i + 1, frequency[i], (frequency[i] / 100.0) * 100);
+        }
+    }
+}
