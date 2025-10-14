61b0eabde98130df08382b0c101301e84862e30b
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-14 23:19:46 +0530

    new file:   Ques_231_to_240/ques231.java

---
+++ Ques_231_to_240/ques231.java
@@ -0,0 +1,22 @@
+package Ques_231_to_240;
+
+// Generate a Word Zigzag Pattern
+
+public class ques231 {
+    public static void main(String[] args) {
+        String word = "ZIGZAG";
+        int height = 3;
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < word.length(); j++) {
+                int position = j % (2 * (height - 1));
+                if (position == i || position == 2 * (height - 1) - i) {
+                    System.out.print(word.charAt(j));
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
