bbf3aec7f2137e40e2fb697c2eb9fd366b754174
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-14 23:21:18 +0530

    new file:   Ques_231_to_240/ques233.java

---
+++ Ques_231_to_240/ques233.java
@@ -0,0 +1,20 @@
+package Ques_231_to_240;
+
+// Generate a Pattern of Numbers in a Cross Shape
+
+public class ques233 {
+    public static void main(String[] args) {
+        int size = 5;
+        
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == j || i + j == size + 1) {
+                    System.out.print(i);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
