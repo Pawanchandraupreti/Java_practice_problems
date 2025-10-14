e3caf1aa53706d84762f2b6721ac423428a40e6d
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-14 23:21:56 +0530

    new file:   Ques_231_to_240/ques234.java

---
+++ Ques_231_to_240/ques234.java
@@ -0,0 +1,18 @@
+package Ques_231_to_240;
+
+// Calculate the Sum of Cubes of Digits
+
+public class ques234 {
+    public static void main(String[] args) {
+        int number = 123;
+        int sum = 0;
+        
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit * digit * digit;
+            number /= 10;
+        }
+        
+        System.out.println("Sum of cubes of digits: " + sum);
+    }
+}
