8b102aaa854fad035489b42279aece2ce951792b
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-20 23:26:15 +0530

    new file:   Ques_231_to_240/ques240.java

---
+++ Ques_231_to_240/ques240.java
@@ -0,0 +1,20 @@
+package Ques_231_to_240;
+
+// Generate a Pattern of Fibonacci Numbers in a Triangle
+
+public class ques240 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int a = 0, b = 1;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a + " ");
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+            System.out.println();
+        }
+    }
+}
