6fd3e40694620b603319a3dfe876272d321c9c89
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-23 22:38:24 +0530

    modified:   Ques_231_to_240/ques240.java

--- Ques_231_to_240/ques240.java
+++ Ques_231_to_240/ques240.java
@@ -7,6 +7,7 @@ public class ques240 {
         int rows = 5;
         int a = 0, b = 1;
         
+        
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(a + " ");
