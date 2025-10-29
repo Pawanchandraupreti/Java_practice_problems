45ce688fb19ec4789398b3c424a49ab088edf92e
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-10-29 23:29:00 +0530

    modified:   Ques_231_to_240/ques238.java

--- Ques_231_to_240/ques238.java
+++ Ques_231_to_240/ques238.java
@@ -11,7 +11,6 @@ public class ques238 {
             product *= num;
         }
         
-        
         double geometricMean = Math.pow(product, 1.0 / numbers.length);
         System.out.printf("Geometric mean: %.2f%n", geometricMean);
     }
