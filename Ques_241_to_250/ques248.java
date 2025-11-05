22a1aecbf23a08bff57706ef62f8c9f7df8574bd
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-11-05 23:19:35 +0530

    new file:   Ques_241_to_250/ques248.java

---
+++ Ques_241_to_250/ques248.java
@@ -0,0 +1,17 @@
+package Ques_241_to_250;
+
+// Calculate the Root Mean Square (RMS) of Numbers
+
+public class ques248 {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        double sumOfSquares = 0;
+        
+        for (double num : numbers) {
+            sumOfSquares += num * num;
+        }
+        
+        double rms = Math.sqrt(sumOfSquares / numbers.length);
+        System.out.printf("Root Mean Square: %.2f%n", rms);
+    }
+}
