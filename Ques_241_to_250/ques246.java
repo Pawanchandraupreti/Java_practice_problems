1bb0362fd6337feffce7f06ac4463032c2b10b6b
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-11-05 23:18:08 +0530

    new file:   Ques_241_to_250/ques246.java

---
+++ Ques_241_to_250/ques246.java
@@ -0,0 +1,19 @@
+package Ques_241_to_250;
+
+// Find the Missing Number in a Sequence
+
+public class ques246 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 5, 6, 7, 8, 9, 10}; // Missing 4
+        int n = numbers.length + 1;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        
+        for (int num : numbers) {
+            actualSum += num;
+        }
+        
+        int missing = expectedSum - actualSum;
+        System.out.println("Missing number: " + missing);
+    }
+}
