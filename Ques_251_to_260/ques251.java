cf5e6b55213723eadcde985f522ca4cd2a5d8770
Pawan <upretipawanchandra@gmail.com>
2025-11-15 22:20:04 +0530

    new file:   Ques_251_to_260/ques251.java

---
+++ Ques_251_to_260/ques251.java
@@ -0,0 +1,17 @@
+package Ques_251_to_260;
+
+// Generate a Word Staircase Pattern
+
+public class ques251 {
+    public static void main(String[] args) {
+        String word = "STAIRCASE";
+        
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(" ".repeat(i) + word.charAt(i));
+        }
+        
+        for (int i = word.length() - 2; i >= 0; i--) {
+            System.out.println(" ".repeat(i) + word.charAt(i));
+        }
+    }
+}
