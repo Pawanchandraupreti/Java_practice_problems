142c4d8e4fb1ab50512872202b78a41d3fce4571
Pawan <upretipawanchandra@gmail.com>
2025-11-23 23:31:50 +0530

    new file:   Ques_251_to_260/ques266.java

---
+++ Ques_251_to_260/ques266.java
@@ -0,0 +1,22 @@
+package Ques_251_to_260;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Find the Duplicate Elements in an Array
+
+public class ques266 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 2, 4, 5, 3, 6, 7, 7};
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+        
+        for (int num : numbers) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+        
+        System.out.println("Duplicate elements: " + duplicates);
+    }
+}
