3d6348af2b056d1b9f75cb6d55885d55dd1d62ab
Pawan <upretipawanchandra@gmail.com>
2025-11-23 23:38:01 +0530

    new file:   Ques_251_to_260/ques259.java

---
+++ Ques_251_to_260/ques259.java
@@ -0,0 +1,24 @@
+package Ques_251_to_260;
+
+import java.util.Scanner;
+
+// Create a Simple Text-Based Vowel Counter
+
+public class ques259 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine().toLowerCase();
+        
+        int vowelCount = 0;
+        String vowels = "aeiou";
+        
+        for (char c : sentence.toCharArray()) {
+            if (vowels.indexOf(c) != -1) {
+                vowelCount++;
+            }
+        }
+        
+        System.out.println("Number of vowels: " + vowelCount);
+    }
+}
