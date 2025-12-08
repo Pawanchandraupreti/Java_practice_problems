e2d7b0a70d59dd4c45041fd41dd0234a749c66c8
Pawan <upretipawanchandra@gmail.com>
2025-12-08 23:08:38 +0530

    new file:   Ques_261_to_270/ques270.java

---
+++ Ques_261_to_270/ques270.java
@@ -0,0 +1,18 @@
+package Ques_261_to_270;
+
+// Generate a Pattern of Triangular Numbers
+
+public class ques270 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int number = 1;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
