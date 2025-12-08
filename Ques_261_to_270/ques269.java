114f921fe27961e4c3904ac5672520b70a2952d5
Pawan <upretipawanchandra@gmail.com>
2025-12-08 23:05:01 +0530

    new file:   Ques_261_to_270/ques269.java

---
+++ Ques_261_to_270/ques269.java
@@ -0,0 +1,24 @@
+package Ques_261_to_270;
+
+import java.util.Scanner;
+
+// Create a Simple Text-Based Consonant Counter
+
+public class ques269 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine().toLowerCase();
+        
+        int consonantCount = 0;
+        String vowels = "aeiou";
+        
+        for (char c : sentence.toCharArray()) {
+            if (Character.isLetter(c) && vowels.indexOf(c) == -1) {
+                consonantCount++;
+            }
+        }
+        
+        System.out.println("Number of consonants: " + consonantCount);
+    }
+}
