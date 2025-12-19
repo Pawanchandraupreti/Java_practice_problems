b57f35ce0d1a253b0cd0c741ef4cdd52cd2d8af8
Pawan <upretipawanchandra@gmail.com>
2025-12-19 23:28:35 +0530

    new file:   Ques_271_to_280/ques275.java

---
+++ Ques_271_to_280/ques275.java
@@ -0,0 +1,18 @@
+// Create a Simple Text-Based Random Sentence Generator
+
+import java.util.Random;
+
+public class ques275 {
+     public static void main(String[] args) {
+        Random rand = new Random();
+        String[] subjects = {"The cat", "A dog", "My friend", "The teacher", "A programmer"};
+        String[] verbs = {"runs", "jumps", "codes", "learns", "teaches"};
+        String[] objects = {"quickly", "in Java", "every day", "with joy", "at school"};
+        
+        String sentence = subjects[rand.nextInt(subjects.length)] + " " +
+                         verbs[rand.nextInt(verbs.length)] + " " +
+                         objects[rand.nextInt(objects.length)] + ".";
+        
+        System.out.println("Generated sentence: " + sentence);
+    }
+}
