dc16cd5f76ff62c2785224c0e209b8bc7e08a578
Pawan <upretipawanchandra@gmail.com>
2025-12-19 23:26:31 +0530

    new file:   Ques_271_to_280/ques273.java

---
+++ Ques_271_to_280/ques273.java
@@ -0,0 +1,24 @@
+// Generate a Pattern of Numbers in a Diamond with Spaces
+public class ques273 {
+    public static void main(String[] args) {
+        int n = 4;
+        
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            System.out.print(" ".repeat(n - i));
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            System.out.print(" ".repeat(n - i));
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
