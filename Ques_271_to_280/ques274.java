cfc5f20746b4c7bed55ccadba3e7d4aeed20c628
Pawan <upretipawanchandra@gmail.com>
2025-12-19 23:27:33 +0530

    new file:   Ques_271_to_280/ques274.java

---
+++ Ques_271_to_280/ques274.java
@@ -0,0 +1,16 @@
+// Calculate the Sum of Natural Numbers Using Formula
+public class ques274 {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = n * (n + 1) / 2;
+        
+        System.out.println("Sum of first " + n + " natural numbers: " + sum);
+        
+        // Verification
+        int verifySum = 0;
+        for (int i = 1; i <= n; i++) {
+            verifySum += i;
+        }
+        System.out.println("Verified sum: " + verifySum);
+    }
+}
