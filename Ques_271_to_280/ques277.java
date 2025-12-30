133773ff787da70af6891c0afde40e08c5a44d6b
Pawan <upretipawanchandra@gmail.com>
2025-12-30 23:53:25 +0530

    new file:   Ques_271_to_280/ques277.java

---
+++ Ques_271_to_280/ques277.java
@@ -0,0 +1,18 @@
+
+// Generate a Pattern of Numbers in a Hollow Triangle
+public class ques277 {
+    public static void main(String[] args) {
+        int rows = 6;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == rows) {
+                    System.out.print(j + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
