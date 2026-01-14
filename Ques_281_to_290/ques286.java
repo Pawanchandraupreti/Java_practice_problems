d00fb17809e14dded900a8b826ca2568390092b9
Pawan <upretipawanchandra@gmail.com>
2026-01-14 23:35:38 +0530

    ques286

---
+++ Ques_281_to_290/ques286.java
@@ -0,0 +1,19 @@
+package Ques_281_to_290;
+
+// Find All Pairs in Array that Sum to a Target Value
+
+public class ques286 {
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15, 3, 6, 8};
+        int target = 10;
+        
+        System.out.println("Pairs that sum to " + target + ":");
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    System.out.println(numbers[i] + " + " + numbers[j] + " = " + target);
+                }
+            }
+        }
+    }
+}
